3368d6b49831b02dd27530f873cbc6b16f508163
Adil' Altynbekov <dlltnbkv@gmail.com>
2025-12-27 09:59:52 +0500

    chore: commit little-changes

---
+++ script.js
@@ -0,0 +1,44 @@
+// Task 1
+const firstName = "Adil";
+const jobTitle = "Junior Developer";
+let yearsOfExperience = 0;
+
+console.log(firstName);
+console.log(jobTitle);
+console.log(yearsOfExperience);
+
+// Task 2
+const birthYear = 2005;
+const isLearningJS = true;
+
+console.log(birthYear, isLearningJS);
+
+// birthYear = 2006;
+
+// Task 3
+let population = 20;
+let halfPopulation = population / 2;
+let isGreaterToFindland = population > 6;
+
+console.log(halfPopulation, isGreaterToFindland);
+
+// Task 4
+// "My name is Adil, I am a Junior Developer with 0 years of experience. I live in a country with 20 million people."
+console.log(
+  `My name is ${firstName}, I am a ${jobTitle} with ${yearsOfExperience} years of experience. I live in a country with ${population} million people.`
+);
+
+let nextProject;
+let completedProject = null;
+console.log(typeof nextProject);
+console.log(typeof completedProject);
+
+let startsInGalaxy = 9007199254740991n;
+console.log(typeof startsInGalaxy);
+
+// let sotalStuff = startsInGalaxy + 10;
+
+let value1 = "5" - 2;
+let value2 = "5" + 2;
+console.log(value1);
+console.log(value2);
