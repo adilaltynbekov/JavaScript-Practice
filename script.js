d693de162634b3c3d5449365e7401907e108203e
Adil' Altynbekov <dlltnbkv@gmail.com>
2025-12-29 19:57:13 +0500

    feat: complete one lecture

--- script.js
+++ script.js
@@ -363,3 +363,26 @@ const protectPassword = (password) => {
   return p1 + p2 + p3 + p4;
 };
 console.log(protectPassword("Adil"));
+
+// Introduction to Arrays
+// Task 1
+const teamMembers = ["Mike", "Will", "Lucas", "Dustin"];
+teamMembers[1] = "Alex";
+console.log(teamMembers);
+console.log(teamMembers[2]);
+console.log(teamMembers.length);
+
+// Task 2
+const currentYear = 2025;
+const userProfile = ["Adil", "Altynbekov", currentYear - 2005, teamMembers];
+console.log(userProfile);
+
+// Task 3
+const prices = [100, 250, 400, 600];
+const applyDiscount = (price) => price * 0.9;
+const discountPrices = [
+  applyDiscount(prices[0]),
+  applyDiscount(prices[1]),
+  applyDiscount(prices[prices.length - 1]),
+];
+console.log(discountPrices);
